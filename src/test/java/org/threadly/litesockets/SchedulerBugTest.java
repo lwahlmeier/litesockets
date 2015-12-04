2d471f4db24bc1093da781f4cdfa8720f7c34d58
Luke Wahlmeier <lwahlmeier@gmail.com>
2015-12-04 13:32:32 -0700

    threadly only version of the scheduling bug

--- src/test/java/org/threadly/litesockets/SchedulerBugTest.java
+++ src/test/java/org/threadly/litesockets/SchedulerBugTest.java
@@ -5,6 +5,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Test;
 import org.threadly.concurrent.PriorityScheduler;
+import org.threadly.concurrent.SingleThreadScheduler;
+import org.threadly.concurrent.future.FutureCallback;
+import org.threadly.concurrent.future.SettableListenableFuture;
 import org.threadly.litesockets.Server.ClientAcceptor;
 import org.threadly.litesockets.tcp.Utils;
 import org.threadly.test.concurrent.TestCondition;
@@ -14,30 +17,69 @@ public class SchedulerBugTest {
   @Test
   public void test() throws Exception {
     final AtomicInteger ai = new AtomicInteger(0);
-    final PriorityScheduler PS = new PriorityScheduler(10);
+    final PriorityScheduler PS = new PriorityScheduler(3);
     ThreadedSocketExecuter TSE = new ThreadedSocketExecuter(PS);
     TSE.start();
     int port = Utils.findTCPPort();
+
     TCPServer server = TSE.createTCPServer("localhost", port);
+
     server.setClientAcceptor(new ClientAcceptor() {
       @Override
       public void accept(Client client) {
         System.out.println("New Client, Scheduling!");
-        PS.scheduleAtFixedRate(new Runnable() {
+        PS.schedule(new Runnable() {
           @Override
           public void run() {
             System.out.println("Looped:"+ai.get());
             ai.incrementAndGet();
-          }}, 10, 10);
+          }}, 10);
       }});
     server.start();
     TCPClient client = TSE.createTCPClient("localhost", port);
     client.connect().get(5000, TimeUnit.MILLISECONDS);
+ 
     new TestCondition(){
       @Override
       public boolean get() {
-        return ai.get() >= 10;
+        return ai.get() >= 1;
       }
-    }.blockTillTrue(5000);
+    }.blockTillTrue(2000);
+    PS.shutdownNow();
+  }
+  
+  @Test
+  public void test2() throws Exception {
+    final AtomicInteger ai = new AtomicInteger(0);
+    final PriorityScheduler PS = new PriorityScheduler(3);
+    final SingleThreadScheduler STS = new SingleThreadScheduler();
+
+    STS.execute(new Runnable() {
+      @Override
+      public void run() {
+        PS.schedule(new Runnable() {
+          @Override
+          public void run() {
+          }}, 10000);    
+      }});
+
+    PS.execute(new Runnable() {
+      @Override
+      public void run() {
+        PS.schedule(new Runnable() {
+          @Override
+          public void run() {
+            System.out.println("Looped:"+ai.get());
+            ai.incrementAndGet();
+            System.out.println("--------------------------------------------------");
+          }}, 10);
+      }});
+ 
+    new TestCondition(){
+      @Override
+      public boolean get() {
+        return ai.get() >= 1;
+      }
+    }.blockTillTrue(2000);
   }
 }
